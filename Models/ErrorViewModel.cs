831be2a60697b8262f660524fd325816b5ddf7c5
vikihs2 <victorhstefanov@gmail.com>
2025-05-07 12:09:12 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RecipeSharingSystem.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
