adc53526c06a135cbc77dc695ad730c52d2a5f48
vikihs2 <victorhstefanov@gmail.com>
2025-06-05 09:43:53 +0300

    Add CreatedBy property to Recipe model and update migration files

---
+++ Migrations/20250605064321_UpdateRecipeModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeSharingSystem.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRecipeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Recipes");
+        }
+    }
+}
--- obj/Debug/net9.0/RecipeSharingSystem.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/RecipeSharingSystem.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeSharingSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a63443c58b6f652aa3c2b848468cd545df56ff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7148f0c4407eaf875f37d1940315177139e8646")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeSharingSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeSharingSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
